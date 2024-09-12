c0db940b6c56d75475edfc7165d421614fd543df
SACHIN <ghoshsachin2301@gmail.com>
2024-09-13 00:49:38 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main
+      className=""
+    >
+      <div >
+        <h1>New project</h1>
+      </div>
+    </main>
+  );
+}
