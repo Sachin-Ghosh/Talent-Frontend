345406645195a9f9c64b963048be8f6fcd34a918
Souvik Mondal <sm6984767@gmail.com>
2024-09-13 14:50:23 +0530

    some changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
 import "@/styles/globals.css";
+import Nav from "@/components/Nav";
+import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/theme-provider"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+  <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+  <Nav/>
+  <Component {...pageProps} className="relative mt-10"/>
+  <Footer/>
+  </ThemeProvider>
+  </>
+
+  )
 }
