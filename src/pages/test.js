5f07d6c74cdc3a5a5b9fc18c1c52cc8b4602dcca
SACHIN <ghoshsachin2301@gmail.com>
2024-09-17 01:19:52 +0530

    resolved errors

--- src/pages/test.js
+++ src/pages/test.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Clock, FileText } from "lucide-react"
 // import TestQuestions from '@/components/Test-questions'
-import TestQuestions from '@/components/Test-questions'
+import TestQuestions from '../components/Test-questions'
 
 export default function TestPage() {
   const [testStarted, setTestStarted] = useState(false)
